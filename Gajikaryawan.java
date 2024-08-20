63181707b2ae37d7ed64b0e4d62b408dcc0e72b7
Muhammad Burhannudin Hamzah <muhammad_hamzah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-20 10:51:40 +0700

    Add files via upload

---
+++ Gajikaryawan.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Gajikaryawan {
+
+    public static void main(String[] args) {
+        double jamKerja, gaji, tarifPerjam, pajak;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan jam Kerja: ");
+        jamKerja = input.nextDouble();
+        System.out.println("Masukkan tarif perjam: ");
+        tarifPerjam = input.nextDouble();
+        System.out.println("Masukkan pajak: ");
+        pajak = input.nextDouble();
+        gaji=jamKerja*tarifPerjam;
+        pajak=(pajak/100)*gaji;
+        gaji=gaji-pajak;
+        System.out.println("gaji :"+gaji);
+        
+    }
+    
+}
